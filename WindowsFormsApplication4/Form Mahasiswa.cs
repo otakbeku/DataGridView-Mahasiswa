79d203a225eccfafa97fbfb14564e2473df10d8c
otakbeku <iceramngl@gmail.com>
2016-11-23 13:35:07 +0700

    Add project files.

---
+++ WindowsFormsApplication4/Form Mahasiswa.cs
@@ -0,0 +1,77 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace WindowsFormsApplication4
+{
+    public partial class Form1 : Form
+    {
+        private BindingList<Mahasiswa> _mhs; //variabel binding
+
+        public Form1()
+        {
+            InitializeComponent();
+            _mhs = new BindingList<Mahasiswa>();
+
+            dataGridMHS.DataSource = _mhs;
+            ClearDatagrid();
+        }
+        /// <summary>
+        /// Menngosongkan datagrid
+        /// </summary>
+        private void ClearDatagrid()
+        {
+            dataGridMHS.Rows.Clear(); //Mengosongkan datagrid
+        }
+
+        /// <summary>
+        /// Button yang akan menambahkan item
+        /// </summary>
+        /// <param name="sender">l</param>
+        /// <param name="e"></param>
+        private void button1_Click(object sender, EventArgs e)
+        {
+            addDataToList();
+         }
+
+       
+        /// <summary>
+        /// Button untuk menghapus item yang dipilih
+        /// </summary>
+        /// <param name="sender"></param>
+        /// <param name="e"></param>
+        private void button2_Click(object sender, EventArgs e)
+        {
+            deleteByRow();
+        }
+
+        /// <summary>
+        /// Method untuk menambahkan item yang sebelumnya telah diisikan pada text box
+        /// </summary>
+        private void addDataToList()
+        {
+            if (textNIM.Text != null && textNama.Text != null)
+            {
+                int nim = Convert.ToInt32(textNIM.Text);
+                _mhs.Add(new Mahasiswa(nim, textNama.Text));
+            }
+        }
+
+        /// <summary>
+        /// Method untuk menghapus item pada row yang pilih
+        /// </summary>
+        private void deleteByRow()
+        {
+            foreach (DataGridViewRow item in this.dataGridMHS.SelectedRows)
+            {
+                dataGridMHS.Rows.RemoveAt(item.Index);
+            }
+        }
+    }
+}
---
+++ WindowsFormsApplication4/Mahasiswa.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace WindowsFormsApplication4
+{
+    public class Mahasiswa
+    {
+        private int _nim;
+        private string _nama;
+        private string _alamat;
+
+        public int NIM
+        {
+            get { return _nim; }
+            set { _nim = value; }
+        }
+
+        public string NamaMahasiswa
+        {
+            get { return _nama; }
+            set { _nama = value; }
+        }
+
+        public string AlamatMahasiswa
+        {
+            get { return _alamat; }
+            set { _alamat = value; }
+        }
+
+        public Mahasiswa()
+        {
+            this._nim = 0;
+            this._nama = "Mahasiswa Baru";
+        }
+
+        public Mahasiswa(int nim, string nama)
+        {
+            this._nim = nim;
+            this._nama = nama;
+        }
+    }
+}
